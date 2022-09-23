f1afe4dfd9a8f9a6a8558e1a61ff0daa1d671b6e
Gao Zhiyuan <alapha23@gmail.com>
2022-09-23 18:02:34 +0900

    get scroll working

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ flashcardMaker = (text, delThisIndex) => {
   const answer = document.createElement('h2');
   const del = document.createElement('i');
 
-  flashcard.className = 'flashcard';
+  flashcard.className = 'mySlides fade';
 
   question.setAttribute("style", "border-top:1px solid red; padding: 15px; margin-top:30px");
   question.textContent = text.my_question;
@@ -89,3 +89,33 @@ image_input.addEventListener("change", function() {
   console.log("after 2")
 });
 /* ====adding my own code right here==========================================*/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
