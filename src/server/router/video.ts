9e4c4118df8976cc363b1a2dbed31fe636c0645e
Zhiyuan Gao <zhiyuan.gao@trilogy.com>
2022-09-29 16:14:27 +0900

    remove console log

--- src/server/router/video.ts
+++ src/server/router/video.ts
@@ -140,7 +140,6 @@ export const videoRouter = createRouter()
           userId: session?.user?.id!,
         },
       });
-      console.log('uid',session?.user?.id);
       await prisma.user.update({
         where: { id: session?.user?.id },
         data: {
