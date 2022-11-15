6d3585c22c4d3483bb4417d43c2bc217dd666142
Kushal Chordiya <kushal.chordiya@trilogy.com>
2022-11-15 11:31:46 +0530

    fix cdk stack

--- aws/app.py
+++ aws/app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from aws.ecs_stack import EcsStack
+from stack.ecs_stack import EcsStack
 
 
 app = cdk.App()
-EcsStack(app, "MemoryappCdkStack",
+EcsStack(app, "MemoryappProdCdkStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
